ac48402b5c30e9bfdaed459d2ccca1752fbde9b2
Kamil Stangret <kamilstangret@wp.pl>
2018-02-14 19:22:55 +0100

    Change solution of the task 3

--- 3/script.js
+++ 3/script.js
@@ -1,6 +1,9 @@
-const average = (...args) => {
+/*const average = (...args) => 
 	const result = args.reduce((a,b) =>{return a + b});
 	return result/args.length;
 };
 
-console.log(average(1,2,3,2));
+console.log(average(1,2,3,2)); */
+
+const average = (...args) => (args.reduce((a,b) => a + b ) / args.length );
+console.log(average(1,2,3,4));
